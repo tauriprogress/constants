eb3f4ceb67c79a2ac20b30aba53f4cbb253d8c70
Ákos <absolitudo@gmail.com>
2019-01-30 03:51:54 +0100

    exports

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const characterClasses = require("./characterClasses");
+const characterRaces = require("./characterRaces");
+const classToSpec = require("./classToSpec");
+const currentContent = require("./currentContent");
+const difficultyLabels = require("./difficultyLabels");
+const durumuId = require("./durumuId");
+const realms = require("./realms");
+const soo = require("./Siege of Orgrimmar");
+const specs = require("./specs");
+const specToClass = require("./specToClass");
+const tot = require("./Throne of Thunder");
 const urls = require("./urls");
+const valuesCorrectSince = require("./valuesCorrectSince");
 
 module.exports = {
-    urls
+    characterClasses,
+    characterRaces,
+    classToSpec,
+    currentContent,
+    difficultyLabels,
+    durumuId,
+    realms,
+    "Siege of Orgrimmar": soo,
+    specs,
+    specToClass,
+    "Throne of Thunder": tot,
+    urls,
+    valuesCorrectSince
 };
