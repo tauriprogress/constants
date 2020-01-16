30123beb860c304bc89d45a1526ab37504f88c80
Absolitudo <absolitudo@gmail.com>
2020-01-17 00:03:44 +0100

    inventory type

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const urls = require("./urls");
 const shortRealms = require("./shortRealms");
 const raceToFaction = require("./raceToFaction");
 const tauriLogBugs = require("./tauriLogBugs");
+const inventoryType = require("./inventoryType");
 
 module.exports = {
     characterClasses,
@@ -27,5 +28,6 @@ module.exports = {
     urls,
     shortRealms,
     raceToFaction,
-    tauriLogBugs
+    tauriLogBugs,
+    inventoryType
 };
