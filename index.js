e1b901990001ce24aeba21101874ed10bbb1a19f
Absolitudo <absolitudo@gmail.com>
2020-09-06 05:02:22 +0200

    refactor

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const characterClasses = require("./characterClasses");
-const characterRaces = require("./characterRaces");
-const classToSpec = require("./classToSpec");
-const currentContent = require("./currentContent");
-const difficultyLabels = require("./difficultyLabels");
-const realms = require("./realms");
-const soo = require("./Siege of Orgrimmar");
-const specs = require("./specs");
-const specToClass = require("./specToClass");
-const tot = require("./Throne of Thunder");
-const urls = require("./urls");
-const shortRealms = require("./shortRealms");
-const raceToFaction = require("./raceToFaction");
-const tauriLogBugs = require("./tauriLogBugs");
-const inventoryType = require("./inventoryType");
-const guildFactionBugs = require("./guildFactionBugs");
-
 module.exports = {
-    characterClasses,
-    characterRaces,
-    classToSpec,
-    currentContent,
-    difficultyLabels,
-    realms,
-    "Siege of Orgrimmar": soo,
-    specs,
-    specToClass,
-    "Throne of Thunder": tot,
-    urls,
-    shortRealms,
-    raceToFaction,
-    tauriLogBugs,
-    inventoryType,
-    guildFactionBugs
+    characterClassNames: require("./characterClassNames"),
+    characterClassToSpec: require("./characterClassToSpec"),
+    characterRaceNames: require("./characterRaceNames"),
+    characterRaceToFaction: require("./characterRaceToFaction"),
+    characterSpecToClass: require("./characterSpecToClass"),
+    difficultyNames: require("./difficultyNames"),
+    itemSlotNames: require("./itemSlotNames"),
+    shortRealms: require("./shortRealms"),
+    tauri: require("./tauri")
 };
