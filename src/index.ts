0e57f292142a48120f79e94e1c6eeeaf95798a17
Absolitudo <absolitudo@gmail.com>
2021-01-16 12:19:17 +0100

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as characterClassToSpec from "./characterClassToSpec.json";
+import * as characterRaceNames from "./characterRaceNames.json";
+import * as characterRaceToFaction from "./characterRaceToFaction.json";
+import * as characterSpecToClass from "./characterSpecToClass.json";
+import * as itemSlotNames from "./itemSlotNames.json";
+import * as shortRealms from "./shortRealms.json";
+import * as raidNameToId from "./raidNameToId.json";
+import * as tauri from "./tauri";
+import * as crystalsong from "./crystalsong";
+
+export { characterClassToSpec };
+export { characterRaceNames };
+export { characterRaceToFaction };
+export { characterSpecToClass };
+export { itemSlotNames };
+export { shortRealms };
+export { raidNameToId };
+export { tauri };
+export { crystalsong };
