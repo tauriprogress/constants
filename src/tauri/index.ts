b863a25f920b857269b7c9b52a79bb2320b7d7ff
Absolitudo <absolitudo@gmail.com>
2021-01-16 12:19:27 +0100

    tauri.ts

---
+++ src/tauri/index.ts
@@ -0,0 +1,17 @@
+import * as currentContent from "./currentContent.json";
+import * as guildFactionBugs from "./guildFactionBugs.json";
+import * as logBugs from "./logBugs.json";
+import * as realms from "./realms.json";
+import * as specs from "./specs.json";
+import * as urls from "./urls.json";
+import * as difficultyNames from "./difficultyNames.json";
+import * as characterClassNames from "./characterClassNames.json";
+
+export { currentContent };
+export { guildFactionBugs };
+export { logBugs };
+export { realms };
+export { specs };
+export { urls };
+export { difficultyNames };
+export { characterClassNames };
