3f41ffee76d12141a8ef1102dfc85ff35fc6e33e
akoskoc <akoskoc@gmail.com>
2022-01-05 13:40:48 +0100

    rename currentcontent fields & test currentcontent

--- src/tauri/currentContent.ts
+++ src/tauri/currentContent.ts
@@ -2,7 +2,7 @@ export default Object.freeze({
     abbreviation: "SOO",
     name: "Siege of Orgrimmar",
     lastBoss: "Garrosh Hellscream",
-    totalBosses: 14,
+    bossCount: 14,
     completionDifficulties: [5, 6],
     raids: [
         {
@@ -15,100 +15,100 @@ export default Object.freeze({
             bosses: [
                 {
                     name: "Immerseus",
-                    difficultyIds: {
-                        "5": 1602,
-                        "6": 1602,
+                    bossIdOfDifficulty: {
+                        5: 1602,
+                        6: 1602,
                     },
                 },
                 {
                     name: "Fallen Protectors",
-                    difficultyIds: {
-                        "5": 1598,
-                        "6": 1598,
+                    bossIdOfDifficulty: {
+                        5: 1598,
+                        6: 1598,
                     },
                 },
                 {
                     name: "Norushen",
-                    difficultyIds: {
-                        "5": 1624,
-                        "6": 1624,
+                    bossIdOfDifficulty: {
+                        5: 1624,
+                        6: 1624,
                     },
                 },
                 {
                     name: "Sha of Pride",
-                    difficultyIds: {
-                        "5": 1604,
-                        "6": 1604,
+                    bossIdOfDifficulty: {
+                        5: 1604,
+                        6: 1604,
                     },
                 },
                 {
                     name: "Galakras",
-                    difficultyIds: {
-                        "5": 1622,
-                        "6": 1622,
+                    bossIdOfDifficulty: {
+                        5: 1622,
+                        6: 1622,
                     },
                 },
                 {
                     name: "Iron Juggernaut",
-                    difficultyIds: {
-                        "5": 1600,
-                        "6": 1600,
+                    bossIdOfDifficulty: {
+                        5: 1600,
+                        6: 1600,
                     },
                 },
                 {
                     name: "Kor'kron Dark Shaman",
-                    difficultyIds: {
-                        "5": 1606,
-                        "6": 1606,
+                    bossIdOfDifficulty: {
+                        5: 1606,
+                        6: 1606,
                     },
                 },
                 {
                     name: "General Nazgrim",
-                    difficultyIds: {
-                        "5": 1603,
-                        "6": 1603,
+                    bossIdOfDifficulty: {
+                        5: 1603,
+                        6: 1603,
                     },
                 },
                 {
                     name: "Malkorok",
-                    difficultyIds: {
-                        "5": 1595,
-                        "6": 1595,
+                    bossIdOfDifficulty: {
+                        5: 1595,
+                        6: 1595,
                     },
                 },
                 {
                     name: "Spoils of Pandaria",
-                    difficultyIds: {
-                        "5": 1594,
-                        "6": 1594,
+                    bossIdOfDifficulty: {
+                        5: 1594,
+                        6: 1594,
                     },
                 },
                 {
                     name: "Thok the Bloodthirsty",
-                    difficultyIds: {
-                        "5": 1599,
-                        "6": 1599,
+                    bossIdOfDifficulty: {
+                        5: 1599,
+                        6: 1599,
                     },
                 },
                 {
                     name: "Siegecrafter Blackfuse",
-                    difficultyIds: {
-                        "5": 1601,
-                        "6": 1601,
+                    bossIdOfDifficulty: {
+                        5: 1601,
+                        6: 1601,
                     },
                 },
                 {
                     name: "Paragons of the Klaxxi",
-                    difficultyIds: {
-                        "5": 1593,
-                        "6": 1593,
+                    bossIdOfDifficulty: {
+                        5: 1593,
+                        6: 1593,
                     },
                 },
                 {
                     name: "Garrosh Hellscream",
-                    difficultyIds: {
-                        "5": 1623,
-                        "6": 1623,
+                    bossIdOfDifficulty: {
+                        5: 1623,
+                        6: 1623,
                     },
                 },
             ],
---
+++ tests/tauri/currentContent.test.js
@@ -0,0 +1,98 @@
+const currentContent = require("../../src/tauri/currentContent").default;
+
+const currentContentFields = [
+    {
+        key: "abbreviation",
+        test: (value) => expect(typeof value).toBe("string"),
+    },
+    { key: "name", test: (value) => expect(typeof value).toBe("string") },
+    { key: "lastBoss", test: (value) => expect(typeof value).toBe("string") },
+    { key: "bossCount", test: (value) => expect(typeof value).toBe("number") },
+    {
+        key: "completionDifficulties",
+        test: (value) => {
+            expect(Array.isArray(value)).toBe(true);
+            for (const diff of value) {
+                expect(typeof diff).toBe("number");
+            }
+        },
+    },
+    {
+        key: "raids",
+        test: (value) => {
+            expect(Array.isArray(value)).toBe(true);
+        },
+    },
+];
+
+const raidFields = [
+    { key: "name", test: (value) => expect(typeof value).toBe("string") },
+    { key: "lastBoss", test: (value) => expect(typeof value).toBe("string") },
+    { key: "bossCount", test: (value) => expect(typeof value).toBe("number") },
+    { key: "id", test: (value) => expect(typeof value).toBe("number") },
+    {
+        key: "difficulties",
+        test: (value) => {
+            expect(Array.isArray(value)).toBe(true);
+            for (const diff of value) {
+                expect(typeof diff).toBe("number");
+            }
+        },
+    },
+    {
+        key: "image",
+        test: (value) => expect(typeof value).toBe("string"),
+    },
+    {
+        key: "bosses",
+        test: (value) => {
+            expect(Array.isArray(value)).toBe(true);
+        },
+    },
+];
+
+const bossFields = [
+    { key: "name", test: (value) => expect(typeof value).toBe("string") },
+    {
+        key: "bossIdOfDifficulty",
+        test: (value) => {
+            expect(value instanceof Object).toBe(true);
+            Object.keys(value).map((key) => {
+                expect(typeof Number(key)).toBe("number");
+                expect(typeof value[key]).toBe("number");
+            });
+        },
+    },
+];
+
+describe("Tauri current content", () => {
+    describe(`Has all required fields`, () => {
+        for (const field of currentContentFields) {
+            test(field.key.toUpperCase(), () => {
+                field.test(currentContent[field.key]);
+            });
+        }
+    });
+
+    describe(`Raids have all required fields`, () => {
+        for (const raid of currentContent.raids) {
+            for (const field of raidFields) {
+                test(field.key.toUpperCase(), () => {
+                    field.test(raid[field.key]);
+                });
+            }
+        }
+    });
+
+    describe(`Bosses have all required fields`, () => {
+        for (const raid of currentContent.raids) {
+            for (const field of bossFields) {
+                for (const boss of raid.bosses) {
+                    test(field.key.toUpperCase(), () => {
+                        field.test(boss[field.key]);
+                    });
+                }
+            }
+        }
+    });
+});
