9d2743adcf0dcad53d73dec58f47b4976c916bcf
akoskoc <akoskoc@gmail.com>
2022-01-05 14:07:03 +0100

    test guildfactionbugs

---
+++ tests/tauri/testVariables.js
@@ -0,0 +1,11 @@
+const realms = [
+    "[HU] Tauri WoW Server",
+    "[EN] Evermoon",
+    "[HU] Warriors of Darkness",
+];
+const factions = [0, 1];
+
+module.exports = {
+    realms,
+    factions,
+};
