cbbd5cd721ca65e193e1be075e33d979ca73a051
akoskoc <akoskoc@gmail.com>
2022-01-06 17:04:27 +0100

    fix types of shared vars

--- src/shared/shortRealms.ts
+++ src/shared/shortRealms.ts
@@ -3,4 +3,9 @@ export default Object.freeze({
     "[HU] Warriors of Darkness": "w",
     "[EN] Evermoon": "e",
     Crystalsong: "c",
-});
+}) as {
+    "[HU] Tauri WoW Server": "t";
+    "[HU] Warriors of Darkness": "w";
+    "[EN] Evermoon": "e";
+    Crystalsong: "c";
+};
