d5ef3a8edf9c2566fa086aac40f18d5d039e027b
akoskoc <akoskoc@gmail.com>
2022-01-06 17:02:42 +0100

    refactor shortrealms

---
+++ tests/shared/shortRealms.test.js
@@ -0,0 +1,22 @@
+const shortRealms = require("../../src/shared/shortRealms").default;
+
+const realms = {
+    "[HU] Tauri WoW Server": "t",
+    "[HU] Warriors of Darkness": "w",
+    "[EN] Evermoon": "e",
+    Crystalsong: "c",
+};
+
+describe("Shared short realms", () => {
+    test(`There are ${Object.keys(realms).length} realms`, () => {
+        expect(Object.keys(shortRealms).length).toBe(
+            Object.keys(realms).length
+        );
+    });
+
+    for (const key in realms) {
+        test(`${key} has id ${realms[key]}`, () => {
+            expect(shortRealms[key]).toBe(realms[key]);
+        });
+    }
+});
