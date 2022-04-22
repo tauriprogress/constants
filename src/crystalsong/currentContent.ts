020b5cb81ddbd3792c80aca06185c4a971d3c32b
akoskoc <akoskoc@gmail.com>
2022-04-22 07:09:51 +0200

    crystalsong icc

--- src/crystalsong/currentContent.ts
+++ src/crystalsong/currentContent.ts
@@ -1,10 +1,128 @@
 export default Object.freeze({
-    abbreviation: "TOC",
-    name: "Trial of the Crusader",
-    lastBoss: "Anub'arak",
-    bossCount: 5,
+    abbreviation: "ICC",
+    name: "Icecrown Citadel",
+    lastBoss: "The Lich King",
+    bossCount: 12,
     completionDifficulties: [3],
     raids: [
+        {
+            name: "Icecrown Citadel",
+            lastBoss: "The Lich King",
+            bossCount: 12,
+            id: 631,
+            difficulties: [0, 1, 2, 3],
+            image: "icc.png",
+            bosses: [
+                {
+                    name: "Lord Marrowgar",
+                    bossIdOfDifficulty: {
+                        0: 845,
+                        1: 857,
+                        2: 845,
+                        3: 857,
+                    },
+                },
+                {
+                    name: "Lady Deathwhisper",
+                    bossIdOfDifficulty: {
+                        0: 846,
+                        1: 858,
+                        2: 846,
+                        3: 858,
+                    },
+                },
+                {
+                    name: "Icecrown Gunship Battle",
+                    bossIdOfDifficulty: {
+                        0: 847,
+                        1: 859,
+                        2: 847,
+                        3: 859,
+                    },
+                },
+                {
+                    name: "Deathbringer Saurfang",
+                    bossIdOfDifficulty: {
+                        0: 848,
+                        1: 860,
+                        2: 848,
+                        3: 860,
+                    },
+                },
+                {
+                    name: "Festergut",
+                    bossIdOfDifficulty: {
+                        0: 849,
+                        1: 861,
+                        2: 849,
+                        3: 861,
+                    },
+                },
+                {
+                    name: "Rotface",
+                    bossIdOfDifficulty: {
+                        0: 850,
+                        1: 862,
+                        2: 850,
+                        3: 862,
+                    },
+                },
+                {
+                    name: "Professor Putricide",
+                    bossIdOfDifficulty: {
+                        0: 851,
+                        1: 863,
+                        2: 851,
+                        3: 863,
+                    },
+                },
+                {
+                    name: "Blood Council",
+                    bossIdOfDifficulty: {
+                        0: 852,
+                        1: 864,
+                        2: 852,
+                        3: 864,
+                    },
+                },
+                {
+                    name: "Queen Lana'thel",
+                    bossIdOfDifficulty: {
+                        0: 853,
+                        1: 865,
+                        2: 853,
+                        3: 865,
+                    },
+                },
+                {
+                    name: "Valithria Dreamwalker",
+                    bossIdOfDifficulty: {
+                        0: 854,
+                        1: 866,
+                        2: 854,
+                        3: 866,
+                    },
+                },
+                {
+                    name: "Sindragosa",
+                    bossIdOfDifficulty: {
+                        0: 855,
+                        1: 867,
+                        2: 855,
+                        3: 867,
+                    },
+                },
+                {
+                    name: "The Lich King",
+                    bossIdOfDifficulty: {
+                        0: 856,
+                        1: 868,
+                        2: 856,
+                        3: 868,
+                    },
+                },
+            ],
+        },
         {
             name: "Trial of the Crusader",
             lastBoss: "Anub'arak",
