72bd1311049e9598fccca330b94f1ba981eee641
akoskoc <akoskoc@gmail.com>
2024-05-05 12:25:31 +0200

    revert mb2 server change

--- src/mistblade2/urls.ts
+++ src/mistblade2/urls.ts
@@ -1,5 +1,5 @@
 export default Object.freeze({
-    server: "https://mb2-2v3i.onrender.com",
+    server: "https://mb2-serv.glitch.me",
     icon: "https://mop-static.tauri.hu/images/icons",
     shoot: "https://mop-shoot.tauri.hu",
     api: "https://characters-api.stormforge.gg/v1/",
