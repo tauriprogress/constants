1f92619bb2dfffd4f6e2c002cf907adcf9920950
akoskoc <akoskoc@gmail.com>
2024-08-14 17:13:17 +0200

    update mistblade 2 current content

--- src/mistblade2/currentContent.ts
+++ src/mistblade2/currentContent.ts
@@ -1,10 +1,118 @@
 export default Object.freeze({
-    abbreviation: "TOT",
-    name: "Throne of Thunder",
-    lastBoss: "Ra-den",
-    bossCount: 13,
+    abbreviation: "SOO",
+    name: "Siege of Orgrimmar",
+    lastBoss: "Garrosh Hellscream",
+    bossCount: 14,
     completionDifficulties: [5, 6],
     raids: [
+        {
+            name: "Siege of Orgrimmar",
+            lastBoss: "Garrosh Hellscream",
+            bossCount: 14,
+            id: 1136,
+            difficulties: [5, 6],
+            image: "soo.png",
+            bosses: [
+                {
+                    name: "Immerseus",
+                    bossIdOfDifficulty: {
+                        5: 1602,
+                        6: 1602,
+                    },
+                },
+                {
+                    name: "Fallen Protectors",
+                    bossIdOfDifficulty: {
+                        5: 1598,
+                        6: 1598,
+                    },
+                },
+                {
+                    name: "Norushen",
+                    bossIdOfDifficulty: {
+                        5: 1624,
+                        6: 1624,
+                    },
+                },
+                {
+                    name: "Sha of Pride",
+                    bossIdOfDifficulty: {
+                        5: 1604,
+                        6: 1604,
+                    },
+                },
+                {
+                    name: "Galakras",
+                    bossIdOfDifficulty: {
+                        5: 1622,
+                        6: 1622,
+                    },
+                },
+                {
+                    name: "Iron Juggernaut",
+                    bossIdOfDifficulty: {
+                        5: 1600,
+                        6: 1600,
+                    },
+                },
+                {
+                    name: "Kor'kron Dark Shaman",
+                    bossIdOfDifficulty: {
+                        5: 1606,
+                        6: 1606,
+                    },
+                },
+                {
+                    name: "General Nazgrim",
+                    bossIdOfDifficulty: {
+                        5: 1603,
+                        6: 1603,
+                    },
+                },
+                {
+                    name: "Malkorok",
+                    bossIdOfDifficulty: {
+                        5: 1595,
+                        6: 1595,
+                    },
+                },
+                {
+                    name: "Spoils of Pandaria",
+                    bossIdOfDifficulty: {
+                        5: 1594,
+                        6: 1594,
+                    },
+                },
+                {
+                    name: "Thok the Bloodthirsty",
+                    bossIdOfDifficulty: {
+                        5: 1599,
+                        6: 1599,
+                    },
+                },
+                {
+                    name: "Siegecrafter Blackfuse",
+                    bossIdOfDifficulty: {
+                        5: 1601,
+                        6: 1601,
+                    },
+                },
+                {
+                    name: "Paragons of the Klaxxi",
+                    bossIdOfDifficulty: {
+                        5: 1593,
+                        6: 1593,
+                    },
+                },
+                {
+                    name: "Garrosh Hellscream",
+                    bossIdOfDifficulty: {
+                        5: 1623,
+                        6: 1623,
+                    },
+                },
+            ],
+        },
         {
             name: "Throne of Thunder",
             lastBoss: "Ra-den",
@@ -198,6 +306,7 @@ export default Object.freeze({
                 },
             ],
         },
+
         {
             name: "Mogu'shan Vaults",
             lastBoss: "Will of the Emperor",
